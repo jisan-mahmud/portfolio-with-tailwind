3d80fdd308b3a17d915e920ee03ebb954345ded3
Jisan Mahmud <jisa.mahmud20@gmail.com>
2024-08-22 15:31:35 +0600

    add github profile section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["*.html"],
-    theme: {
-      extend: {
-        
-      },
-    },
-    plugins: [],
-  };
-  
+  content: ["*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
