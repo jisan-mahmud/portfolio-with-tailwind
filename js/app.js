6d708de53221bb81a9c20538e28940a60b841e05
Jisan Mahmud <jisa.mahmud20@gmail.com>
2024-08-24 15:50:04 +0600

    responsive mobile menu bar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 let x = 0;
-function menu_ber(){
+function menu_ber(event){
     let menu_items = document.getElementById('menu_items');
+    let menu_icon = document.querySelector('.menu_icon')
+    console.log(menu_icon)
     if(x == 0){
         menu_items.classList.remove('hidden')
+        menu_icon.classList.remove('fa-bars')
+        menu_icon.classList.add('fa-xmark')
         x = 1
     }else{
         menu_items.classList.add('hidden')
+        menu_icon.classList.add('fa-bars')
+        menu_icon.classList.remove('fa-xmark')
         x = 0
     }
 }
+
+let github_profile_load = () => {
+    fetch('https://api.github.com/users/jisan-mahmud')
+    .then(res => {
+        return res.json();
+    })
+    .then(json => {
+        github_profile_display(json)
+    })
+    
+}
+
+let github_profile_display = (json) =>{
+    
+}
+
+github_profile_load()
